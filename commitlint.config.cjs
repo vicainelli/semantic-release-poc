3605b6ae6a05c70eea24723916644437034382ca
Vinicius Cainelli <vinicius.cainelli@ingka.ikea.com>
2025-06-06 13:43:21 +0200

    chore: update unit tests s

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,18 @@
 module.exports = {
 	extends: ["@commitlint/config-conventional"],
+	parserPreset: {
+		parserOpts: {
+			// Scope is optional, allows optional space after type, and enforces jira ticket
+			headerPattern:
+				/^([a-z]+) ?(?:\(([a-z0-9-]+)\))?: ([a-z]{2,5}-\d{1,8}) (.+)$/,
+			headerCorrespondence: ["type", "scope", "jira", "subject"],
+		},
+	},
+	rules: {
+		"type-case": [2, "always", "lower-case"],
+		"scope-case": [2, "always", "lower-case"],
+		"type-empty": [0], // Disable misleading error
+		"subject-empty": [0], // Disable misleading error
+		// Removed invalid rule: "header-pattern-required"
+	},
 };
